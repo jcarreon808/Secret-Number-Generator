b1add122e7e835081f585ea23fab9b6070b90f66
Jan Phillip Carreon <janphillipcarreon@Jans-MacBook-Pro.local>
2016-09-07 19:10:06 -1000

    finished secret number

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,8 @@
 'use strict';
 module.exports = function() {
-  
+    var secretNumberGenerator = Math.floor((Math.random()*1000000)+1);
+   function secretNumber(){
+    return secretNumberGenerator;
+  }
+  return secretNumber;
 };
